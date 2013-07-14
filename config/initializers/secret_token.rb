20f857299df3fb57587ae8d8eb36ae5193ab32e0
TH <tylerhartland7@gmail.com>
2013-07-14 14:53:59 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeafGrandma::Application.config.secret_key_base = '2c7b6e6ee60b8a8d8bfa1f6fbffaa693815776a76112918eae991a5252afd4eeab6d504385c2b317991dcc2ebe01cd729766ed32983fcba5fc55ed184c7f24dc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DeafGrandma::Application.config.session_store :cookie_store, key: '_deaf-grandma_session'
