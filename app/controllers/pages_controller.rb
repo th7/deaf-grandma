cb34b18b8ab59fcd46b13401ae78a4dc93ca4ed4
TH <tylerhartland7@gmail.com>
2013-07-14 15:30:38 -0500

    basic reaction

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,21 @@
 class PagesController < ApplicationController
-  
+  skip_before_filter :verify_authenticity_token  
+
+  def get
+    input = params[:input] if params[:input]
+
+    if input
+      bye = "I love ya, Grandma, but I've got to go."
+
+      if input == bye.upcase
+        @grandma = 'OK, SONNY! SEE YA!'
+      elsif input == input.upcase
+        @grandma = "NO, NOT SINCE 1983!"
+      else
+        @grandma = "HUH?! SPEAK UP, SONNY!"
+      end
+    else
+      @grandma = "HELLO, SONNY! HOW ARE YOU?"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 DeafGrandma::Application.routes.draw do
-  get '/' => 'pages#index'
+  get '/' => 'pages#get'
+  post '/grandma' => 'pages#get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
